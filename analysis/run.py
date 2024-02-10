3b7f3361eca362cc9b779087e971f485bea0c788
estevesjh <johnny.udi@gmail.com>
2024-02-10 10:37:01 -0600

    move to nersc

--- analysis/run.py
+++ analysis/run.py
@@ -15,7 +15,7 @@ root = '/data/des61.a/data/johnny/DESY3/projects/buzzardAllHalos/CopacabanaRuns/
 cfg  = root+'cf_all.yaml'
 copa = copacabana(cfg,dataset='buzzard_v2')
 
-copa.make_input_file()
+#copa.make_input_file()
 
 # def write_indices_out(indices,fname,col='02Lstar',overwrite=False):
 #     fmaster = h5py.File(fname, 'a')
@@ -57,22 +57,26 @@ copa.make_input_file()
 # for fname in copa.master_fname_tile_list:
 #     apply_02Lstar_cut(fname)
 
-# # ##### Atention: this cell takes more than 6 hours to run. It only needs to be run one time.
-# # copa.run_bma_healpix(nCores=63,overwrite=True)
+# ##### Atention: this cell takes more than 6 hours to run. It only needs to be run one time.
+# copa.run_bma_healpix(nCores=63,overwrite=True)
 
-# ### nohup python run.py > log.out 2> log.err &
+### nohup python run.py > log.out 2> log.err &
 
 # ## Photo-z
-# # generate_photoz_models('gaussian',copa.master_fname_tile_list,0.01,nCores=60)
-# # generate_photoz_models('gaussian',copa.master_fname_tile_list,0.03,nCores=60)
-# # generate_photoz_models('gaussian',copa.master_fname_tile_list,0.05,nCores=60)
+# generate_photoz_models('gaussian',copa.master_fname_tile_list,0.01,nCores=60)
 
-# # outfile1=None#root+'aux_files/modelDNF_correction_mag_buzzard.txt'
-# # outfile2=root+'aux_files/modelDNF_correction_z_buzzard.txt'
+# print('gauss003')
+# generate_photoz_models('gaussian',copa.master_fname_tile_list,0.03,nCores=60)
 
-# # generate_photoz_models('bias',copa.master_fname_tile_list,0.03,
-# #                        zwindow_file=outfile2,zerror_file=outfile1,
-# #                        group_name='dnf',nCores=60)
+# print('gauss05)
+# generate_photoz_models('gaussian',copa.master_fname_tile_list,0.05,nCores=60)
+
+# outfile1=None#root+'aux_files/modelDNF_correction_mag_buzzard.txt'
+# outfile2=root+'aux_files/modelDNF_correction_z_buzzard.txt'
+
+# generate_photoz_models('bias',copa.master_fname_tile_list,0.03,
+#                        zwindow_file=outfile2,zerror_file=outfile1,
+#                        group_name='dnf',nCores=60)
 
 # # outfile1=root+'aux_files/modelDNF_correction_mag_buzzard.txt'
 # # outfile2=root+'aux_files/modelDNF_correction_z_buzzard.txt'
@@ -83,54 +87,54 @@ copa.make_input_file()
 
 # ## Copa Run
 # # setup 
-# copa.kwargs['r_aper_model'] = 'rhod'
-# copa.kwargs['mag_selection']= '02Lstar'
-# ## runs
-# pz_files = ['gauss001','gauss003','gauss005']
-# z_widths = [0.01,0.03,0.05]
-# runs = ['%s_%s_%s'%(pz,'rhod','02Lstar') for pz in pz_files]
+copa.kwargs['r_aper_model'] = 'rhod'
+copa.kwargs['mag_selection']= '02Lstar'
+## runs
+pz_files = ['gauss001','gauss003','gauss005']
+z_widths = [0.01,0.03,0.05]
+runs = ['%s_%s_%s'%(pz,'rhod','02Lstar') for pz in pz_files]
 
-# pz_files += ['dnf_model','dnf']
-# z_widths += [0.03,0.03]
-# runs     += ['%s_%s_%s'%(pz,'rhod','02Lstar') for pz in pz_files[3:]]
+pz_files += ['dnf']
+z_widths += [0.03]
+runs     += ['%s_%s_%s'%(pz,'rhod','02Lstar') for pz in pz_files[3:]]
 
-# for run, zfile, zw in zip(runs,pz_files,z_widths):
-#     print(5*'---')
-#     print('run     : %s'%run)
-#     print('zfile   : %s'%zfile)
-#     print('zwindow : %.2f'%zw)
-#     print(5*'---')
-#     if zfile!='gauss001':
-#         copa.kwargs['z_window'] = zw
-#         #copa.run_copa_healpix(run,   pz_file=zfile, nCores=60)
-#         copa.compute_muStar(run, overwrite=True)
-#         print(5*'---')
-#         print('\n')
+for run, zfile, zw in zip(runs,pz_files,z_widths):
+    print(5*'---')
+    print('run     : %s'%run)
+    print('zfile   : %s'%zfile)
+    print('zwindow : %.2f'%zw)
+    print(5*'---')
+    if zfile!='gauss001':
+        copa.kwargs['z_window'] = zw
+        copa.run_copa_healpix(run,   pz_file=zfile, nCores=60)
+        #copa.compute_muStar(run, overwrite=True)
+        print(5*'---')
+        print('\n')
 
-# ## setup 
-# copa.kwargs['r_aper_model'] = 'r200'
-# copa.kwargs['mag_selection']= '02Lstar'
-# ## runs
-# pz_files = ['gauss001','gauss003','gauss005']
-# z_widths = [0.01,0.03,0.05]
+## setup 
+copa.kwargs['r_aper_model'] = 'r200'
+copa.kwargs['mag_selection']= '02Lstar'
+## runs
+pz_files = ['gauss001','gauss003','gauss005']
+z_widths = [0.01,0.03,0.05]
 
-# runs = ['%s_%s_%s'%(pz,'r200','02Lstar') for pz in pz_files]
-# pz_files += ['dnf_model','dnf']
-# z_widths += [0.03,0.03]
-# runs     += ['%s_%s_%s'%(pz,'r200','02Lstar') for pz in pz_files[3:]]
+runs = ['%s_%s_%s'%(pz,'r200','02Lstar') for pz in pz_files]
+pz_files += ['dnf']
+z_widths += [0.03]
+runs     += ['%s_%s_%s'%(pz,'rhod','02Lstar') for pz in pz_files[3:]]
 
-# for run, zfile, zw in zip(runs,pz_files,z_widths):
-#     print(5*'---')
-#     print('run     : %s'%run)
-#     print('zfile   : %s'%zfile)
-#     print('zwindow : %.2f'%zw)
-#     print(5*'---')
+for run, zfile, zw in zip(runs,pz_files,z_widths):
+    print(5*'---')
+    print('run     : %s'%run)
+    print('zfile   : %s'%zfile)
+    print('zwindow : %.2f'%zw)
+    print(5*'---')
     
-#     copa.kwargs['z_window'] = zw
-#     #copa.run_copa_healpix(run,   pz_file=zfile, nCores=60)
-#     copa.compute_muStar(  run, overwrite=True)
-#     print(5*'---')
-#     print('\n')
+    copa.kwargs['z_window'] = zw
+    #copa.run_copa_healpix(run,   pz_file=zfile, nCores=60)
+    #copa.compute_muStar(  run, overwrite=True)
+    print(5*'---')
+    print('\n')
 
-# tf = (time()-t0)/60.
-# print('final time %.2f hours'%(tf/60.))
+tf = (time()-t0)/60.
+print('final time %.2f hours'%(tf/60.))
